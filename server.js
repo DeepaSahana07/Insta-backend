168cd10bbe1b95bc554e033729ff211ef51e1b36
Deepa Sahana <deepasahan@gmail.com>
2025-12-18 03:46:32 +0530

    5  commit

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ app.use((req, res, next) => {
 app.options('*', cors());
 
 // Routes
-app.use('/api/user-auth', authRoutes);
+app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/posts', postRoutes);
 
