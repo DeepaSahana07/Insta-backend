c1c1cfb85080d9250a92b1d1cb100a2f8b84560e
Deepa Sahana <deepasahan@gmail.com>
2025-12-18 03:18:30 +0530

    5  commit

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -6,7 +6,7 @@ import { upload } from '../middleware/uploadMiddleware.js';
 const router = express.Router();
 
 router.post('/register', register);
-router.post('/login', login);
+router.post('/signin', login);
 router.post('/verify-password', verifyPassword);
 router.get('/me', protect, getMe);
 
--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.use((req, res, next) => {
 app.options('*', cors());
 
 // Routes
-app.use('/api/auth', authRoutes);
+app.use('/api/user-auth', authRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/posts', postRoutes);
 
