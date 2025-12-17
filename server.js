ffbe5c434da698dd977627588a38abe8e131fe79
Deepa Sahana <deepasahan@gmail.com>
2025-12-18 03:26:29 +0530

    5  commit

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: ['https://insta-frontend-rouge-five.vercel.app', 'http://localhost:3000', 'http://localhost:3001', 'http://localhost:5173'],
+  origin: ['https://insta-frontend-rouge-five.vercel.app', 'https://insta-frontend-4842hjg9g-deepasahana07s-projects.vercel.app', 'http://localhost:3000', 'http://localhost:3001', 'http://localhost:5173'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
